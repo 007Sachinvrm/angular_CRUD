ddf14425b4ec1ac05b3dd3f1bbd297f214ea7c79
github_username <email_address>
2024-10-17 18:07:31 +0530

    logged in user name visible on header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,8 +11,15 @@ import { PLATFORM_ID } from '@angular/core';
 })
 export class HeaderComponent {
 
+  userName: string | null = '';
+
   constructor(private http: HttpClient, private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngOnInit(): void {
+    // Load user details on initialization
+    this.loadUserName();
+  }
+  
   isLoggedIn(): boolean {
     // Check if the code is running in the browser
     if (isPlatformBrowser(this.platformId)) {
@@ -26,5 +33,15 @@ export class HeaderComponent {
     this.router.navigate(['/register-login']);
   }
 
+  loadUserName(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const user = localStorage.getItem('user');
+      if (user) {
+        const userData = JSON.parse(user);
+        this.userName = userData?.name || 'User';  // Assuming the user's name is stored as 'name'
+        console.log(this.userName);
+      }
+    }
+  }
 
 }
